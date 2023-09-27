ab3c749c07a4534089847604fe129212ff39516c
joaomanoel0 <joaosobreira@alu.ufc.br>
2023-09-26 21:37:53 -0300

    dados

---
+++ gatway.py
@@ -0,0 +1,7 @@
+import descoberta_pb2
+
+mensagem_descoberta = descoberta_pb2.MensagemDescoberta()
+mensagem_descoberta.device_id = '1'
+mensagem_descoberta.device_type = 0
+mensagem_descoberta.ip_address = '127.0.0.1'
+mensagem_descoberta.port = 21203
