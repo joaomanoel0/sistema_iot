62591724e2475e1176d5ccefc4f068304d64432e
João Manoel <84255075+joaomanoel0@users.noreply.github.com>
2023-10-08 21:41:10 -0300

    Comentários no código code_esp.ino

--- parte2/esp_embarcado/code_esp.ino
+++ parte2/esp_embarcado/code_esp.ino
@@ -3,29 +3,27 @@
 #include <DHT.h>
 #include <Arduino.h>
 
-#include <Adafruit_Sensor.h>
+//bibliotecas do socket e rede
 #include <WebSocketsClient_Generic.h>
 #include <WebSocketsServer_Generic.h>
 #include <stdlib.h>
-
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
+//bibliotecas do nanopb
 #include "proto_mensagens.pb.h"
 #include "pb_common.h"
 #include "pb_decode.h"
 #include "pb_encode.h"
-char *strTemp;
 
 const char *ssid = "brisa-2664316";
 const char *password = "kum0hxa8";
 // const char* ssid = "JM";
 // const char* password = "testedoprojeto2";
 
-// Porta que a ESP8266 ficará escutando
+// Porta que a ESP8266 ficará escutando o multicast
 const IPAddress multicast_ip(224, 1, 1, 1);
 unsigned int multi_port = 54321;
-//unsigned int port_udp = 12345;
 unsigned int device_port = 8078;
 unsigned int gateway_port;
 IPAddress ip_esp;
@@ -40,8 +38,8 @@ float humidity;     //Variável para armazenar a umidade
 char buffer_sensor[101];
 String sensor_tu = "";
 
-#define DHT_PIN D2      // Pino onde o DHT22 está conectado (por exemplo, D2).
-#define DHT_TYPE DHT22  // Tipo de sensor DHT que você está usando (DHT11, DHT21, ou DHT22).
+#define DHT_PIN D2     
+#define DHT_TYPE DHT22  
 
 DHT dht(DHT_PIN, DHT_TYPE);
 
@@ -188,21 +186,18 @@ void envia_umidade(DeviceToGatewayMessage mensagem, WiFiUDP server_socket_send) 
       Serial.println("Temperatura e umidade enviadas com sucesso");
     }
     verifica_parada();
-    //delay(1000);
   }
 }
 
 void verifica_parada() {
   tempo_ex = millis();
   WiFiUDP server_socket_receve2;
-  //server_socket_receve2.begin(8078);
   u_int8_t buffer6[128];
   int sizepk2;
   envia = true;
   while(millis() - tempo_ex < 2000 || envia == false){
     sizepk2 = server_socket_receve.parsePacket();
     if(sizepk2){
-    //Serial.println("ok");
     int tamanho = server_socket_receve.read(buffer6, sizeof(buffer6));
     Serial.println(tamanho);
     GatewayToDeviceMessage mensagemGattoDev_pare;
